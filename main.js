348ab1a837e4ad5922d356c8aa27ffba24145cc9
sddzcuigc <85976753+sddzcuigc@users.noreply.github.com>
2021-07-14 09:47:30 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ if (window.location.href.indexOf("login") == -1) {
     if (window.location.href.indexOf("courseId") == -1) {
         window.location.href = "https://gbwlxy.dtdjzx.gov.cn/content#/myClass?tbtpId=3487301376055194246&title=%E5%85%A8%E7%9C%81%E5%B9%B2%E9%83%A8%E6%94%BF%E5%BE%B7%E6%95%99%E8%82%B2%E4%B8%93%E9%A2%98%E5%9F%B9%E8%AE%AD%E7%8F%AD";
         setTimeout(function(){
-            document.getElementsByClassName("el-button el-button--text el-button--small")[6].click()
+            document.getElementsByClassName("el-button el-button--text el-button--small")[0].click()
         }, 10000);
     }
     else {
@@ -10,7 +10,7 @@ if (window.location.href.indexOf("login") == -1) {
         console.log(ti);
         if (ti < 1) {
             setTimeout(function(){window.history.back(-1);
-                                  }, 10000);
+                                  }, 2000);
             setTimeout(function(){window.location.reload();
                                   }, 10000);
         }
